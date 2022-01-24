76c28367011fbc61fd39d71fc8e65a5fb0304d15
Joel Berglund <joelberglund.mail@gmail.com>
2022-01-24 15:18:37 +0100

    Section 11: 77-Refactor-Bootstrap-Data

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @Builder
 @Entity
-public class User  implements UserDetails, CredentialsContainer {
+public class User implements UserDetails, CredentialsContainer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,7 +28,7 @@ public class User  implements UserDetails, CredentialsContainer {
     private String password;
 
     @Singular
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
--- src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
@@ -1,7 +1,10 @@
 package guru.sfg.brewery.repositories.security;
 
+import java.util.Optional;
+
 import guru.sfg.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String customer);
 }
