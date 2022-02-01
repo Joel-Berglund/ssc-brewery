7b104bfb7a27f888ae62aa913ebff15d2256ca62
Joel Berglund <joelberglund.mail@gmail.com>
2022-02-01 16:14:04 +0100

    Section 16: 113-Generate-QR-Code-for-Google-Authenticator

--- src/main/java/guru/sfg/brewery/config/SecurityBeans.java
+++ src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -1,6 +1,11 @@
 package guru.sfg.brewery.config;
 
 
+import java.util.concurrent.TimeUnit;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
+import com.warrenstrange.googleauth.ICredentialRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +21,22 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class SecurityBeans {
+
+    @Bean
+    public GoogleAuthenticator googleAuthenticator(ICredentialRepository credentialRepository){
+        GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder configBuilder
+                = new GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder();
+
+        configBuilder
+                .setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(60))
+                .setWindowSize(10)
+                .setNumberOfScratchCodes(0);
+
+        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator(configBuilder.build());
+        googleAuthenticator.setCredentialRepository(credentialRepository);
+        return googleAuthenticator;
+
+    }
 
     @Bean
     public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher){
