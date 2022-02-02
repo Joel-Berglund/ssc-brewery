3f00c2ee1506907f1bfc91533dd9ddba22f36a3f
Joel Berglund <joelberglund.mail@gmail.com>
2022-02-02 14:12:24 +0100

    Section 17: 126-Spring-Security-CORS-Configuration

--- src/main/java/guru/sfg/brewery/SfgBreweryUiApplication.java
+++ src/main/java/guru/sfg/brewery/SfgBreweryUiApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
 public class SfgBreweryUiApplication {
 
     public static void main(String[] args) {
